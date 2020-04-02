9fe39e11bd7a1220447efef3c500a40864748923
X@Pointer <pro.vikas.tandon@gmail.com>
2020-04-02 17:55:40 +0900

    Binary Search

---
+++ src/main/java/com/my/ic/ds/BinarySearch.java
@@ -0,0 +1,36 @@
+package com.my.ic.ds;
+
+/**
+ * @author vikas_tandon
+ *
+ */
+public class BinarySearch {
+	
+	public static boolean binarySearch(int srchEle, int arry[]) {
+		
+		int startIndex=-1;
+		int endIndex=arry.length;
+		while(startIndex+1<endIndex) {
+			int len=endIndex-startIndex;
+			int mid = len/2;
+			int midIndex = startIndex+mid;
+			int midEle = arry[midIndex];
+			if(midEle == srchEle) {
+				return true;
+			}
+			if(midEle > srchEle) {
+				endIndex = midIndex;
+			}else {
+				startIndex=midIndex;
+			}
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		int arry[] = {1,2,3,4,5,6,7,8,9,10};
+		int srchEle = 1;
+		System.out.println(binarySearch(srchEle,arry));
+	}
+
+}
