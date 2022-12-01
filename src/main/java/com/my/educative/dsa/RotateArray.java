b1edc2d44f9540d73eb4451aa474d6699deafb5a
X@Pointer <4018280+GitPointer@users.noreply.github.com>
2022-12-01 15:02:54 +0000

    Co-authored-by: Keshav Kishore <keshavkishore09@users.noreply.github.com>

---
+++ src/main/java/com/my/educative/dsa/RotateArray.java
@@ -0,0 +1,27 @@
+package com.my.educative.dsa;
+
+public class RotateArray {
+
+    public static void main(String[] args){
+        int[] arr={1, 2, 3, 4, 5};
+        //(1,2,3,4,5)->5
+        //(5,1,2,3,4,)
+        
+
+       new RotateArray().rotateArray(arr);
+       for(int i:arr){
+        System.out.println(i);
+       }
+    }
+
+    
+    void rotateArray(int[] arr){
+        int lastEle = arr[arr.length-1];
+        for (int index = arr.length-1; index > 0 ; index--) {
+             arr[index] = arr[index-1];
+        }
+        arr[0] = lastEle;
+
+    }
+
+}
