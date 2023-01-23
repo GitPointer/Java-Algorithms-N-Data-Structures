e2731c3ae07b8c7f66ffe761942115b9d878be5b
X@Pointer <4018280+GitPointer@users.noreply.github.com>
2023-01-23 15:16:44 +0000

    Co-authored-by: Keshav Kishore <keshavkishore09@users.noreply.github.com>

--- src/main/java/com/my/educative/dsa/graph/CycleUsingBFS.java
+++ src/main/java/com/my/educative/dsa/graph/CycleUsingBFS.java
@@ -3,12 +3,12 @@ package com.my.educative.dsa.graph;
 import com.my.educative.dsa.linkedlist.DoublyLinkedListWithTail;
 import com.my.educative.dsa.queue.MyQueue;
 
-public class BFS {
-    public static String bfs(Graph g){
-      String result = "";
+public class CycleUsingBFS {
+    public static boolean bfs(Graph g){
+      boolean result = false;
       //Checking if the graph has no vertices
       if (g.vertices < 1){
-          return result;
+          return false;
       }
       //Boolean Array to hold the history of visited nodes (by default-false)
       boolean[] visited = new boolean[g.vertices];
@@ -17,13 +17,13 @@ public class BFS {
           //Checking whether the node is visited or not
           if(!visited[i])
           {
-              result = result + bfsVisit(g, i, visited);
+              result =  bfsVisit(g, i, visited);
           }
       }
       return result;
   }
-  public static String bfsVisit(Graph g, int source, boolean[] visited) {
-      String result = "";
+  public static boolean bfsVisit(Graph g, int source, boolean[] visited) {
+      boolean result = false;
       //Create Queue for Breadth First Traversal and enqueue source in it
       MyQueue<Integer> queue = new MyQueue<>(g.vertices);
       queue.enqueue(source);
@@ -32,7 +32,6 @@ public class BFS {
       while (!queue.isEmpty()) {
           //Dequeue a vertex/node from queue and add it to result
           int current_node = queue.dequeue();
-          result += String.valueOf(current_node);
           //Get adjacent vertices to the current_node from the array,
           //and if they are not already visited then enqueue them in the Queue
           DoublyLinkedListWithTail<Integer>.Node temp = null;
@@ -42,6 +41,8 @@ public class BFS {
               if (!visited[temp.data]) {
                   queue.enqueue(temp.data);
                   visited[temp.data] = true; //Visit the current Node
+              } else {
+                result = true;
               }
               temp = temp.nextNode;
           }
@@ -49,22 +50,13 @@ public class BFS {
       return result;
   }
   public static void main(String args[]) {
-      Graph g = new Graph(5);
+      Graph g = new Graph(3);
       g.addEdges(0,1);
-      g.addEdges(0,2);
-      g.addEdges(1,3);
-      g.addEdges(1,4);
+      g.addEdges(1,2);
+      g.addEdges(2,3);
       System.out.println("Graph1:");
       g.printGraph();
-      System.out.println("BFS traversal of Graph1 : " + bfs(g));
+      System.out.println("Is Cyclic Graph : " + bfs(g));
       System.out.println();
-      Graph g2 = new Graph(5);
-      g2.addEdges(0,1);
-      g2.addEdges(0,4);
-      g2.addEdges(1,2);
-      g2.addEdges(3,4);
-      System.out.println("Graph2:");
-      g2.printGraph();
-      System.out.println("BFS traversal of Graph2 : " + bfs(g2));
 }
 }
---
+++ src/main/java/com/my/educative/dsa/graph/DFS.java
@@ -0,0 +1,71 @@
+package com.my.educative.dsa.graph;
+
+import java.util.Stack;
+
+import com.my.educative.dsa.linkedlist.DoublyLinkedListWithTail;
+import com.my.educative.dsa.stack.MyStack;
+
+public class DFS {
+
+    Stack<Integer> stack  = new Stack<Integer>();
+    public static void main(String[] args) {
+        Graph g = new Graph(5);
+        g.addEdges(0,1);
+        g.addEdges(0,2);
+        g.addEdges(1,3);
+        g.addEdges(1,4);
+        System.out.println("Graph1:");
+        g.printGraph();
+        System.out.println("DFS traversal of Graph1 : " + dfs(g));
+        System.out.println();
+        System.out.println("This graph is cyclic? : " + isCycle(g));
+    }
+
+    private static String dfs(Graph g) {
+        String result = "";
+        //Checking if the graph has no vertices
+        if (g.vertices < 1){
+            return result;
+        }
+
+        //Boolean Array to hold the history of visited nodes (by default-false)
+        boolean[] visited = new boolean[g.vertices];
+    
+        for(int i=0; i<g.vertices; i++) 
+        { 
+            //Checking whether the node is visited or not 
+            if(!visited[i]) 
+            { 
+                result = result + dfsVisit(g, i, visited); 
+            } 
+        }
+        return result;
+    }
+
+    public static String dfsVisit(Graph g, int source, boolean[] visited) {
+        String result = "";
+        //Create Stack for Depth First Traversal and push source in it
+        MyStack<Integer> stack = new MyStack<>(g.vertices);
+        stack.push(source);
+        visited[source] = true;
+        //Traverse while Stack is not empty
+        while (!stack.isEmpty()) {
+            //Pop a vertex/node from stack and add it to result
+            int current_node = stack.pop();
+            result += String.valueOf(current_node);
+            //Get adjacent vertices to the current_node from the array,
+            //and if they are not already visited then push them in the Stack
+            DoublyLinkedListWithTail<Integer>.Node temp = null;
+            if(g.adjacentList[current_node] != null)
+                temp = g.adjacentList[current_node].headNode;
+            while (temp != null) {
+                if (!visited[temp.data]) {
+                    stack.push(temp.data);
+                    visited[temp.data] = true; //Visit the current Node
+                }
+                temp = temp.nextNode;
+            }
+        }//end of while
+        return result;
+    }
+}
